c66a523069f61b9a9931c95cfa2d865c6a264431
Jelena Petrović <petrovic.it34.2021@uns.ac.rs>
2025-10-09 22:15:19 +0200

    have added background services schedule tasks and send mail function

---
+++ BackgroundServices/helpers/sendMail.js
@@ -0,0 +1,31 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
+
+function createTransporter(config) {
+  const transpoter = nodemailer.createTransport(config);
+  return transpoter;
+}
+let configurations = {
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  requireTls: true,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+};
+
+const sendMail = async (messageoption) => {
+  const transpoter = await createTransporter(configurations);
+  await transpoter.verify();
+  await transpoter.sendMail(messageoption, (error, info) => {
+    if (error) {
+      console.log(error);
+    }
+    console.log(info.response);
+  });
+};
+
+export default sendMail;
