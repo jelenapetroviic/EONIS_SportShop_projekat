f69fe9bfbf7aae00710d9339ad6e18ba30b482b5
Jelena Petrović <petrovic.it34.2021@uns.ac.rs>
2025-10-12 16:01:21 +0200

    started frontent

---
+++ frontend/src/App.jsx
@@ -0,0 +1,49 @@
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Myaccount from "./pages/Myaccount";
+import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
+import Announcement from "./components/Announcement";
+
+function App() {
+  const Layout = () => {
+    return (
+      <div>
+        <Announcement />
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children:[
+        {
+          path:"/",
+          element: <Home />
+        },
+        {
+          path:"/cart",
+          element: <Cart />
+        },
+        {
+          path:"/myaccount",
+          element: <Myaccount />
+        }
+      ]
+    },
+    
+  ]);
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
