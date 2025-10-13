3c53e0aa386d40da616635b6faea3d7254b34724
Jelena Petrović <petrovic.it34.2021@uns.ac.rs>
2025-10-13 12:03:39 +0200

    completed homepage

---
+++ frontend/src/components/Banner.jsx
@@ -0,0 +1,22 @@
+const Banner = () => {
+  return (
+    <div className="relative bg-[url('/beautybanner2.jpg')] bg-no-repeat bg-cover h-[80vh] px-[200px]">
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+
+      <div className="relative flex flex-col text-white w-[50%] pt-[10%]">
+        <span className="text-[30px] mt-3">
+          Discover Your Radiance with Our Handpicked Beauty Essentials
+          
+          <h1 className="text-3x1 mt-3">Glow with Confidence</h1>
+
+          <div className="flex items-center mt-[20px] space-x-8">
+            <button className="bg-[#e48bcd] py-[16px] px-[55px] w-[250px] text-[19px] text-white cursor-pointer hover:bg-[#d06eb8] transition-all duration-300 shadow-md">Shop Now</button>
+            <button className="bg-gray-600 py-[16px] px-[55px] w-[280px] text-[19px] text-white cursor-pointer hover:bg-gray-500 transition-all duration-300 shadow-md">CALL: (176) 678 890</button>
+          </div>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,14 @@
+import Banner from "../components/Banner"
+import Category from "../components/Category"
+import Products from "../components/Products"
+
 const Home = () => {
   return (
     <div>
-      Home
+      <Banner />
+      <Category />
+      <Products />
+      
     </div>
   )
 }
